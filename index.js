4888137f51e4deb843a61145f4db7fe5028e0d22
Jana Khanji <jana.khanji@ieee.org>
2020-11-16 11:43:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 function greet(time){
-  let hour=time.split(":")[0].parseInt;
+  let hour=time.split(":")[0];
   console.log(hour);
   if(hour<=12){
     return "Good Morning";
