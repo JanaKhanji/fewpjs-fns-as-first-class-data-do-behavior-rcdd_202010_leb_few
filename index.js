e6584ce53ae54c93f382629356d13a00ca8aaea4
Jana Khanji <jana.khanji@ieee.org>
2020-11-16 11:43:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ function handleClick(e) {
 /* Write your implementation of greet() */
 function greet(time){
   let hour=time.split(":")[0];
-  console.log(hour);
   if(hour<=12){
     return "Good Morning";
   }
